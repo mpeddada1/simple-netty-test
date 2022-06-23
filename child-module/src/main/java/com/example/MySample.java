1a2a3980c4b6a9c6a397212a517b79de467a307f
Mridula Peddada <mpeddada@google.com>
2022-06-22 20:52:54 -0400

    narrow down the SimpleLogger problem to netty dependency

--- child-module/src/main/java/com/example/MySample.java
+++ child-module/src/main/java/com/example/MySample.java
@@ -1,9 +1,15 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MySample {
+
+  private final static Logger LOGGER = LoggerFactory.getLogger(MySample.class);
 
   public static void main(String... args) {
     System.out.println("HELLLOOO");
+    LOGGER.info("HELLLOO!");
   }
 
 }
